3f77bc353a9b2c3be73ee2ad0e929f343831eb71
Ciprian Constantin <ciprianfc@gmail.com>
2024-05-03 08:47:52 +0300

    angular stuff working with tailwind

--- lua/plugins/angular.lua
+++ lua/plugins/angular.lua
@@ -1,54 +1,54 @@
 return { -- correctly setup lspconfig
-  -- { "elgiano/nvim-treesitter-angular", branch = "topic/jsx-fix" },
-  -- { "elgiano/nvim-treesitter-angular"},
   -- { "nvim-treesitter/nvim-treesitter-angular", branch = "feat-control-flow" },
+  -- {" dlvandenberg/tree-sitter-angular"},
+  { "elgiano/nvim-treesitter-angular", branch = "topic/jsx-fix" },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = { -- make sure mason installs the server
+  --     servers = {
+  --       angularls = {
+  --         filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "angular.html" },
+  --       },
+  --       -- this is needed here for tailwind to work
+  --       html = {
+  --         filetypes = {
+  --           "angular.html",
+  --           "html",
+  --           "templ",
+  --           "tmpl",
+  --           "template",
+  --         },
+  --       },
+  --     },
+  --     setup = {
+  --       angularls = function(_, opts)
+  --         vim.filetype.add({
+  --           pattern = {
+  --             [".*%.component%.html"] = "angular.html", -- Sets the filetype to `angular.html` if it matches the pattern
+  --           },
+  --         })
+  --
+  --         vim.api.nvim_create_autocmd("FileType", {
+  --           pattern = "angular.html",
+  --           callback = function()
+  --             vim.treesitter.language.register("angular", "angular.html") -- Register the filetype with treesitter for the `angular` language/parser
+  --           end,
+  --         })
+  --       end,
+  --     },
+  --   },
+  -- },
   {
-    "neovim/nvim-lspconfig",
-    opts = { -- make sure mason installs the server
-      servers = {
-        angularls = {
-          filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "angular.html" },
-        },
-        -- this is needed here for tailwind to work
-        html = {
-          filetypes = {
-            "angular.html",
-            "html",
-            "templ",
-            "tmpl",
-            "template",
-          },
-        },
-      },
-      setup = {
-        angularls = function(_, opts)
-          vim.filetype.add({
-            pattern = {
-              [".*%.component%.html"] = "angular.html", -- Sets the filetype to `angular.html` if it matches the pattern
-            },
-          })
-
-          vim.api.nvim_create_autocmd("FileType", {
-            pattern = "angular.html",
-            callback = function()
-              vim.treesitter.language.register("angular", "angular.html") -- Register the filetype with treesitter for the `angular` language/parser
-            end,
-          })
-        end,
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
+    -- "nvim-treesitter/nvim-treesitter",
     -- opts = {
     --   ensure_installed = {
     --     "angular",
     --   },
     -- },
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "angular",
-      })
-    end,
+    -- opts = function(_, opts)
+    --   vim.list_extend(opts.ensure_installed, {
+    --     "angular",
+    --   })
+    -- end,
   },
 }
